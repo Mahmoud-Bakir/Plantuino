f2aef0174c745f9e271f8555ff55c401f41491dc
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-16 12:18:04 +0300

    <FE APP> Created NavigationScreen Component

---
+++ Client/screens/NavigationScreen/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View,} from "react-native";
+import BottomTabNavigator from "../../Components/BottomTabNavigator";
+
+
+export default function NavigationScreen() {
+  return (
+    <>
+     <BottomTabNavigator />
+    </>
+     
+  );
+}
