c9cf60b6b4e9a51e34964b3b66bae78bb07ee575
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-16 23:10:31 +0300

    <FE APP> Created Message Component

---
+++ Client/Components/Message/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import colors from "../../assets/colors/colors";
+import { useFonts } from "expo-font";
+export default function Message({ type, message,time }) {
+  const [fontsLoaded] = useFonts({
+    "Raleway-Bold": require("../../assets/fonts/Raleway-Bold.ttf"),
+    "Raleway-Regular": require("../../assets/fonts/Raleway-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+  return (
+    <View
+      style={[
+        styles.userMessageContainer,
+        type == "bot" && styles.botMessageContainer,
+      ]}
+    >
+      <View style={styles.contentContainer}>
+        <Text style={[styles.message, type == "bot" && styles.botMessage]}>
+          {message}
+        </Text>
+        <Text style={[styles.userTimeStamp, type == "bot" && styles.botTimeStamp]}>
+          {time}
+        </Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  userMessageContainer: {
+    backgroundColor: colors.DarkGrey,
+    borderTopLeftRadius: 25,
+    borderBottomLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingVertical: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  botMessageContainer: {
+    backgroundColor: colors.LightBlue,
+    borderBottomRightRadius: 25,
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    borderBottomLeftRadius: 0,
+    paddingVertical: 15,
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+  contentContainer: {
+    gap: 5,
+  },
+  message: {
+    color: colors.White,
+    fontFamily: "Raleway-Regular",
+    fontSize: 14,
+  },
+  botMessage: {
+    color: colors.Black,
+    fontFamily: "Raleway-Regular",
+    fontSize: 14,
+  },
+  userTimeStamp: {
+    fontFamily: "Raleway-Regular",
+    fontSize: 10,
+    color: colors.LightGrey,
+  },
+  botTimeStamp: {
+    fontFamily: "Raleway-Regular",
+    fontSize: 10,
+    color: colors.Grey,
+    alignSelf: "flex-end",
+    paddingRight: 20,
+  },
+});
