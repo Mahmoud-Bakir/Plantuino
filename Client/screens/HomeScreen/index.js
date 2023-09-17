8ff1c33b07a20d13e4b34e697fd0acbdbacd13fb
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-17 13:25:11 +0300

    <FE APP> Introduced ScrollView in HomeScreen

--- Client/screens/HomeScreen/index.js
+++ Client/screens/HomeScreen/index.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import colors from "../../assets/colors/colors";
 import ScreenHeader from "../../Components/ScreensHeader";
 import { useFonts } from "expo-font";
+import { useState } from "react";
 import { LargeButton } from "../../Components/Buttons/LargeButton";
 import Home from "../../assets/pictures/homeLabel.svg";
 import Toggle from "../../Components/Toggle";
+import SearchInput from "../../Components/SearchInput";
+import PlantCard from "../../Components/PlantCard";
 
 export default function HomeScreen() {
+  const [selectedChoice, setSelectedChoice] = useState("My Garden");
   const [fontsLoaded] = useFonts({
     "Raleway-Bold": require("../../assets/fonts/Raleway-Bold.ttf"),
     "Raleway-Regular": require("../../assets/fonts/Raleway-Regular.ttf"),
   });
+  const handleChoiceSelection = (choice) => {
+    setSelectedChoice(choice);
+    console.log(choice);
+  };
 
   if (!fontsLoaded) {
     return <Text>Loading...</Text>;
@@ -25,21 +39,42 @@ export default function HomeScreen() {
           choice1="My Garden"
           choice2="Market"
           style={styles.toggle}
+          onChoiceSelected={handleChoiceSelection}
         />
       </View>
-      <View style={styles.contentContainer}>
-        <Text style={styles.title}>No Plants</Text>
-        <Text style={styles.subtitle}>
-          Start by connecting your arduino kit
-        </Text>
-        <LargeButton title={"Connect"} />
-      </View>
+      {selectedChoice === "My Garden" ? (
+        <ScrollView>
+          <SafeAreaView style={styles.container}>
+            <View style={styles.contentContainer}>
+              <Text style={styles.title}>No Plants</Text>
+              <Text style={styles.subtitle}>
+                Start by connecting your arduino kit
+              </Text>
+              <LargeButton title={"Connect"} />
+            </View>
+          </SafeAreaView>
+        </ScrollView>
+      ) : (
+        <>
+          <View style={styles.search}>
+            <SearchInput />
+          </View>
+          <ScrollView style={styles.scroll}>
+            <View style={styles.productsContainer}>
+              <PlantCard />
+              <PlantCard />
+              <PlantCard />
+            </View>
+          </ScrollView>
+        </>
+      )}
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: 1000,
   },
   contentContainer: {
     flex: 1,
@@ -61,7 +96,23 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
   toggleContainer: {
+    marginTop: 30,
     marginHorizontal: 20,
     alignSelf: "center",
   },
+  search: {
+    alignItems: "center",
+    marginHorizontal: 50,
+    marginTop: 20,
+  },
+  productsContainer: {
+    marginTop: 20,
+    marginHorizontal: 20,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap:12
+  },
+  scroll:{
+    marginTop:20
+  }
 });
