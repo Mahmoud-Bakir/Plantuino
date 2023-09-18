8549137026a394e2fd9f5af99490642c57165dd8
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-19 02:37:51 +0300

    <BE> Testing AWS

--- Server/index.js
+++ Server/index.js
@@ -2,20 +2,20 @@ const express = require("express");
 require("dotenv").config();
 const mongooseConnect = require("./configs/mongoDB.connect");
 const app = express();
-app.use(express.json());
 
-const authRouter = require("./routes/auth.routes")
-app.use("/auth", authRouter)
+app.use(express.json());
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
 const authMiddleware = require("./middlewares/auth.middleware");
 
 const usersRouter = require("./routes/user.routes");
-app.use("/users", authMiddleware, usersRouter)
+app.use("/users", authMiddleware, usersRouter);
 
-app.listen(8000, (err) => {
+app.listen(80, (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log("server running on port: ", 8000);
+  console.log("server running on port: ", 80);
   mongooseConnect();
 });
