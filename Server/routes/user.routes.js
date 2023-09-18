27c439357dd4691591930e47645c8226f17a7029
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-19 02:17:04 +0300

    <BE> Testing on Server

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/user.controllers");
 
 
-router.get("/market", userController.getMarketPlants)
+// router.get("/publicMarket", userController.publicMarket)
+router.get("/personalMarket", userController.personalMarket)
 router.post("/add", userController.addProduct)
 
 
