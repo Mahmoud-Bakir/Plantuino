a8b96fd9f2f0f5674392dd773f3389dff69084dd
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-19 22:14:24 +0300

    <FE APP> Edited Navigation in app.js

--- Client/App.js
+++ Client/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import RegisterationScreen from './screens/RegisterationScreen';
-import SigninScreen from './screens/SigninScreen'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import RegisterationScreen from "./screens/RegisterationScreen";
+import SigninScreen from "./screens/SigninScreen";
+import { StatusBar } from "expo-status-bar";
+import BottomTabNavigator from "./Components/BottomTabNavigator";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+function Home() {
+  return (
+    <BottomTabNavigator/>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor="black"/>
       <Stack.Navigator>
-      <Stack.Screen name="RegisterScreen" component={RegisterationScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="SigninScreen" component={SigninScreen} options={{ headerShown: false }} />
-
+        <Stack.Screen
+          name="SigninScreen"
+          component={SigninScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RegisterScreen"
+          component={RegisterationScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
