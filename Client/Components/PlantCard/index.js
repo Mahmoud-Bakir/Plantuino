4cbc66f9681240613987492f4b2fe84a85879e64
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-21 21:26:14 +0300

    <FE APP> Updated PlantCard Component

--- Client/Components/PlantCard/index.js
+++ Client/Components/PlantCard/index.js
@@ -3,7 +3,13 @@ import { View, StyleSheet, Text, Image } from "react-native";
 import colors from "../../assets/colors/colors";
 import { useFonts } from "expo-font";
 
-export default function PlantCard({ name, price, destination }) {
+export default function PlantCard({
+  name,
+  price,
+  destination,
+  imageUrl,
+  result = false,
+}) {
   const [fontsLoaded] = useFonts({
     "Raleway-Bold": require("../../assets/fonts/Raleway-Bold.ttf"),
     "Raleway-Regular": require("../../assets/fonts/Raleway-Regular.ttf"),
@@ -12,15 +18,24 @@ export default function PlantCard({ name, price, destination }) {
   if (!fontsLoaded) {
     return <Text>Loading...</Text>;
   }
-
+  if (result == true) {
+    return (
+      <View style={styles.cardContainer}>
+        <View style={styles.imageContainer}>
+          <Image source={imageUrl} style={styles.image} resizeMode="contain" />
+        </View>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.price}>{price} $</Text>
+          <Text style={styles.description}>{destination}</Text>
+        </View>
+      </View>
+    );
+  }
   return (
     <View style={styles.cardContainer}>
       <View style={styles.imageContainer}>
-        <Image
-          source={require("../../assets/pictures/example.jpg")}
-          style={styles.image}
-          resizeMode="contain"
-        />
+        <Image source={imageUrl} style={styles.image} resizeMode="contain" />
       </View>
       <View style={styles.detailsContainer}>
         <Text style={styles.name}>{name}</Text>
@@ -34,15 +49,15 @@ export default function PlantCard({ name, price, destination }) {
 const styles = StyleSheet.create({
   cardContainer: {
     width: 180,
-    height: 350, 
+    height: 350,
     backgroundColor: colors.LightBlue,
     borderRadius: 20,
   },
   imageContainer: {
-    height: 270, 
+    height: 270,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    overflow: "hidden", 
+    overflow: "hidden",
   },
   image: {
     flex: 1,
@@ -52,7 +67,7 @@ const styles = StyleSheet.create({
   },
   detailsContainer: {
     padding: 10,
-    gap:2,
+    gap: 2,
   },
   name: {
     fontFamily: "Raleway-Bold",
