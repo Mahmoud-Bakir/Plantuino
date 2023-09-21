9aca0477aea762e6d7e4464303f7203808949585
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-21 21:06:41 +0300

    <FE APP> Edited UserMarket Component

--- Client/Components/UserMarket/index.js
+++ Client/Components/UserMarket/index.js
@@ -2,58 +2,17 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet, ActivityIndicator } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import PlantCard from "../PlantCard";
-import * as SecureStore from "expo-secure-store";
-import axios from "axios";
 import colors from "../../assets/colors/colors";
+import { useSelector } from "react-redux";
+import { selectProducts } from "../../Redux/Store/productSlice";
 
-export default function UserMarket({ refresh = false }) {
-  console.log(refresh);
-  const [products, setProducts] = useState([]);
-  const [id, setId] = useState("");
-  const [userType, setUserType] = useState();
+export default function UserMarket() {
+  const products = useSelector(selectProducts);
   const navigation = useNavigation();
 
   useEffect(() => {
-    console.log("useEffect is running ")
-    const getData = async () => {
-      try {
-        const userType = await SecureStore.getItemAsync("userType");
-        setUserType(userType);
-        const id = await SecureStore.getItemAsync("_id");
-        setId(id);
-        const verify = await SecureStore.getItemAsync("token");
-        const token = verify.slice(1, -1);
-        if (userType == 1) {
-          const response = await axios.get(
-            "http://192.168.1.5:8000/users/personalMarket",
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-          const data = response.data.products;
-          setProducts(data);
-        } else {
-          const response = await axios.get(
-            "http://192.168.1.5:8000/users/publicMarket",
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-          const data = response.data;
-          setProducts(data);
-        }
-      } catch (error) {
-        console.error("Error fetching plants:", error);
-      }
-    };
-
-    getData();
-  }, [refresh]);
-
+    console.log("useEffect is running ");
+  }, []);
   return (
     <>
       {products.length === 0 ? (
@@ -85,8 +44,8 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
     gap: 12,
   },
-  loadingContainer:{
-    alignItems:"center",
-    justifyContent:"center"
-  }
+  loadingContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
