ec728a04c8a8de959f0f65cdc565b64ff36a7474
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-21 15:04:46 +0300

    <FE APP> Added setProducts in Actions

--- Client/Redux/Actions/index.js
+++ Client/Redux/Actions/index.js
@@ -1,8 +1,15 @@
-import { SET_USER_INFO } from '../Types';
+import { SET_USER_INFO, SET_PRODUCTS } from './types';
 
 export const setUserInfo = (userInfo) => {
   return {
     type: SET_USER_INFO,
     payload: userInfo,
+  };
+};
+
+export const setProducts = (products) => {
+  return {
+    type: SET_PRODUCTS,
+    payload: products,
   };
 };
