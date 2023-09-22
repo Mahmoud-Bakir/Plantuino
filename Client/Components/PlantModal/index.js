e77828290dfb62d8c588188c88c7178d8d222da2
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-22 21:15:42 +0300

    <FE APP> Edited PlantModel Component

--- Client/Components/PlantModal/index.js
+++ Client/Components/PlantModal/index.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { View, StyleSheet, TouchableOpacity, Modal, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  Text,
+  SafeAreaView,
+} from "react-native";
 import PlantCard from "../PlantCard";
 import { AntDesign } from "@expo/vector-icons";
 
-const PlantModal = ({ name, destination, price, imageUrl, closeModal, visible }) => {
+const PlantModal = ({
+  name,
+  destination,
+  price,
+  imageUrl,
+  closeModal,
+  visible,
+  phoneNumber
+  
+}) => {
   return (
     <Modal animationType="slide" transparent={true} visible={visible}>
       <View style={styles.modalContainer}>
@@ -12,11 +28,12 @@ const PlantModal = ({ name, destination, price, imageUrl, closeModal, visible })
             <AntDesign name="close" size={25} color="Black" />
           </TouchableOpacity>
           <PlantCard
-            result={true}
+            contact={true}
             name={name}
             price={price}
             imageUrl={imageUrl}
             destination={destination}
+            phoneNumber={phoneNumber}
           />
         </SafeAreaView>
       </View>
