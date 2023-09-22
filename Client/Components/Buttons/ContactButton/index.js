45e3123f1d58a337dace46e29ffd4af6cd0ef4de
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-22 21:16:50 +0300

    <FE APP> Edited ContactButton Component

--- Client/Components/Buttons/ContactButton/index.js
+++ Client/Components/Buttons/ContactButton/index.js
@@ -1,34 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity,Linking  } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Colors from "../../../assets/colors/colors";
 import { useFonts } from "expo-font";
 import Whatsapp from "../../../assets/pictures/Whatsapp.svg"
-export const ContactButton = ({ title, handle, phoneNumber}) => {
+
+export const ContactButton = ({ title,handle}) => {
   const [fontsLoaded] = useFonts({
     "Raleway-SemiBold": require("../../../assets/fonts/Raleway-SemiBold.ttf"),
   });
-  const openWhatsAppChat = () => {
-    const message = 'Hello! I saw this on Plantuino'; 
-    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
-  
-    Linking.canOpenURL(whatsappUrl).then(supported => {
-      if (supported) {
-        return Linking.openURL(whatsappUrl);
-      } else {
-        console.log("WhatsApp is not installed on the device");
-      }
-    });
-  };
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={openWhatsAppChat}>
-        <View style={styles.details}>
-        <Text style={styles.customButton} onPress={handle}>
-          {title}
-        </Text>
-        <Whatsapp/>
-        </View>
-  
+    <View style={styles.container} >
+      <TouchableOpacity onPress={handle} style={styles.logo_container}>
+        <Whatsapp style={styles.logo} />
+        <Text style={styles.customButton}>{title}</Text>
       </TouchableOpacity>
     </View>
   );
@@ -40,22 +24,23 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 20,
     backgroundColor: Colors.Black,
+    width:180,
     borderRadius: 5,
-    paddingHorizontal:5,
+    alignSelf:"center",
+    paddingHorizontal:20,
+    paddingVertical:5
   },
   customButton: {
     textAlign: "center",
     fontSize: 18,
     color: Colors.White,
     padding: 10,
-    width: 110,
-    height: 40,
     fontFamily: "Raleway-SemiBold",
   },
-  details:{
-    paddingHorizontal:5,
-    flexDirection:"row",
+  logo_container:{
+    justifyContent:"center",    
+    width:150,
     alignItems:"center",
-    justifyContent:"center"
+    flexDirection: 'row',
   }
 });
