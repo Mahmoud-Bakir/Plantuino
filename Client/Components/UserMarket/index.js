e49158c51298310c2dd3eb57a80b4a71dfc63405
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-22 16:05:51 +0300

    <FE APP> Implemented PlantModal in UserMarket

--- Client/Components/UserMarket/index.js
+++ Client/Components/UserMarket/index.js
@@ -1,18 +1,36 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, ActivityIndicator } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import PlantCard from "../PlantCard";
 import colors from "../../assets/colors/colors";
 import { useSelector } from "react-redux";
 import { selectProducts } from "../../Redux/Store/productSlice";
+import PlantModal from "../PlantModal";
 
 export default function UserMarket() {
   const products = useSelector(selectProducts);
   const navigation = useNavigation();
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedPlant, setSelectedPlant] = useState(null);
+
+  const handlePress = (plant) => {
+    setSelectedPlant(plant);
+    setModalVisible(true);
+  };
 
   useEffect(() => {
     console.log("useEffect is running ");
   }, []);
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <>
       {products.length === 0 ? (
@@ -22,15 +40,27 @@ export default function UserMarket() {
       ) : (
         <View style={styles.productsContainer}>
           {products.map((plant, index) => (
-            <PlantCard
-              name={plant.name}
-              price={plant.price}
-              destination={plant.destination}
-              image_url={`data:image/jpeg;base64,${plant.image_url}`}
-              key={index}
-            />
+            <TouchableOpacity key={index} onPress={() => handlePress(plant)}>
+              <PlantCard
+                name={plant.name}
+                price={plant.price}
+                destination={plant.destination}
+                imageUrl={plant.imageUrl}
+              />
+            </TouchableOpacity>
           ))}
         </View>
+      )}
+
+      {selectedPlant && (
+        <PlantModal
+          name={selectedPlant.name}
+          imageUrl={selectedPlant.imageUrl}
+          destination={selectedPlant.destination}
+          price={selectedPlant.price}
+          closeModal={closeModal}
+          visible={isModalVisible}
+        />
       )}
     </>
   );
