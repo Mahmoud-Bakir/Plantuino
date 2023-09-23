f0f33c9ee8fdded4f80c9d0985d57b5e12413983
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-23 20:01:11 +0300

    <BE> Created UpdateAdress API

--- Server/controllers/user.controllers.js
+++ Server/controllers/user.controllers.js
@@ -24,8 +24,7 @@ const addProduct = async (req, res) => {
 
     const id = req.user._id;
     const user = await User.findById(id);
-    const userPhoneNumber=user.phoneNumber
-
+    const userPhoneNumber = user.phoneNumber;
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
@@ -36,7 +35,7 @@ const addProduct = async (req, res) => {
       price,
       location,
       imageUrl: "req.file.path",
-      userId:id,
+      userId: id,
       userPhoneNumber,
     };
 
@@ -54,4 +53,38 @@ const addProduct = async (req, res) => {
   }
 };
 
-module.exports = { addProduct, publicMarket, personalMarket };
+const deleteAllUsers = async (req, res) => {
+  try {
+    await User.deleteMany({});
+    res.json({ message: "All users deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateAddress = async (req, res) => {
+  try {
+    const { country, city, street } = req.body;
+    const id = req.user._id;
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.country = country;
+    user.city = city;
+    user.street = street;
+    user.located=true;
+
+    await user.save();
+
+    res.json({ message: "Address updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+module.exports = { addProduct, publicMarket, personalMarket,deleteAllUsers,updateAddress};
