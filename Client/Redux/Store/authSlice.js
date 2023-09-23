0968f23b672f69aa944898e4b439c84d2de76c1e
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-23 21:41:58 +0300

    <FE APP> FIxed Redux

--- Client/Redux/Store/authSlice.js
+++ Client/Redux/Store/authSlice.js
@@ -1,30 +1,51 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  email: '',
-  userType:"",
-  token: '',
-  name: '',
-  phoneNumber: '',
-  id: '',
+  email: "",
+  userType: "",
+  token: "",
+  name: "",
+  phoneNumber: "",
+  id: "",
+  country: "",
+  city: "",
+  street: "",
+  located:false,
 };
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
     setAuthData(state, action) {
-      const { email, token, name, phoneNumber, id , userType } = action.payload;
+      const {
+        email,
+        token,
+        name,
+        phoneNumber,
+        id,
+        userType,
+        located,
+      } = action.payload;
       state.email = email;
       state.token = token;
       state.name = name;
       state.phoneNumber = phoneNumber;
       state.id = id;
-      state.userType=userType
+      state.userType = userType;
+      state.located=located
+    },
+    setAddressData(state, action) {
+      const { city, country, street } = action.payload;
+      state.city = city;
+      state.country = country;
+      state.street = street;
     },
   },
 });
 
-export const { setAuthData } = authSlice.actions;
+export const { setAuthData, setAddressData } = authSlice.actions; 
+
 export const selectAuthState = (state) => state.auth;
+
 export default authSlice.reducer;
