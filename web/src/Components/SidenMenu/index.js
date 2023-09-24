966c707b363355d7b8e2e3e5ea50d61d8f2a12a7
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-24 20:12:34 +0300

    <FE WEB> Created SideMenu Component

---
+++ web/src/Components/SidenMenu/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+import DashboardIcon from "../../assets/pictures/dashboard.svg";
+const SideMenu = () => {
+  return (
+    <div className="sideMenuContainer">
+      <div className="sideMenuContent"></div>
+    </div>
+  );
+};
+
+export default SideMenu;
