ae9aa82ddc05664cbe3c09cf83950f039b038b14
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-24 23:50:39 +0300

    <FE WEB> Updated SideMenu Component

--- web/src/Components/SideMenu/index.js
+++ web/src/Components/SideMenu/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import DashboardIcon from "../../assets/pictures/dashboard.svg";
 import Users from "../../assets/pictures/users.svg";
@@ -6,34 +6,40 @@ import PlantOwners from "../../assets/pictures/PlantOwners.svg";
 import Sellers from "../../assets/pictures/Sellers.svg";
 import AddUser from "../../assets/pictures/addUser.svg";
 
-const SideMenu = () => {
+const SideMenu = ({handleChoice}) => {
+
+  const choose = (choice) => {
+    handleChoice(choice);
+  };
+
   return (
     <div className="sideMenuContainer">
       <div className="sideMenuContent">
-      <div className="categoryContainer">
+        <div className="categoryContainer" onClick={() => choose("Dashboard")}>
           <div className="menuLogoContainer">
             <img src={DashboardIcon} alt="Logo" className="logo" />
           </div>
-          <span >Dashboard</span>
+          <span>Dashboard</span>
         </div>
-      <div className="categoryContainer">
+        <div className="categoryContainer" onClick={() => choose("All Users")} >
           <div className="menuLogoContainer">
             <img src={Users} alt="Logo" className="logo" />
           </div>
-          <span >All Users</span>
+          <span>All Users</span>
         </div>
-        <div className="categoryContainer">
+        <div className="categoryContainer" onClick={() => choose("Sellers")}>
           <div className=" menuLogoContainer">
             <img src={Sellers} alt="Logo" className="logo" />
           </div>
           <span>Sellers</span>
         </div>
-        <div className="categoryContainer">
+        <div className="categoryContainer" onClick={() => choose("Plant Owners")}>
           <div className=" menuLogoContainer">
             <img src={PlantOwners} alt="Logo" className="logo" />
           </div>
           <span>Plant Owners</span>
-        </div> <div className="categoryContainer">
+        </div>{" "}
+        <div className="categoryContainer" onClick={() => choose("Add User")}>
           <div className=" menuLogoContainer">
             <img src={AddUser} alt="Logo" className="logo" />
           </div>
