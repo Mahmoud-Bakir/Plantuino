e1b3cbde87ce419fb22f454d2664189fb87bbba1
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-24 15:16:41 +0300

    <FE WEB> Created Dashboard Page

---
+++ web/src/Pages/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../../Components/NavBar";
+import "../Dashboard/style.css";
+
+const Dashboard = () => {
+  return (
+    <div className="container">
+      <NavBar />
+    </div>
+  );
+};
+
+export default Dashboard;
