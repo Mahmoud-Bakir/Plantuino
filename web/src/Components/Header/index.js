5a5db872515216bc025f7ca9482ac5a4f667deae
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-25 00:39:33 +0300

    <FE WEB> Created Header Component

---
+++ web/src/Components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Header = ({quantity,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11}) => {
+  return (
+      <tr>
+      <th>{t1}</th>
+      <th>{t2}</th>
+      <th>{t3}</th>
+      <th>{t4}</th>
+      <th>{t5}</th>
+      <th>{t6}</th>
+      <th>{t7}</th>
+
+    </tr>
+   
+  )
+} 
+export default Header
