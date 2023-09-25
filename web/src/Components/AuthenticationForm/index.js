7d7bc112414b6a7ac427a93f7fe748a5dea56377
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-25 21:13:04 +0300

    <FE WEB> Updated AuthenticationForm Component

--- web/src/Components/AuthenticationForm/index.js
+++ web/src/Components/AuthenticationForm/index.js
@@ -4,51 +4,69 @@ import LabeledInput from "../LabeledInput";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+
 const AuthenticationForm = () => {
   const navigater = useNavigate();
-  const moveToHome = () => navigater("/home");
+  const moveToDashboard = () => navigater("/Dashboard");
 
   const defaultState = {
     email: "",
     password: "",
   };
   const [data, setData] = useState(defaultState);
+  const [error, setError] = useState("");
   const handleDataChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
+    setError("");
     console.log(data);
   };
   const handleLogin = async () => {
-    const response = await axios.post("http://localhost:8000/auth/login", data);
-    window.localStorage.setItem("token", response.data.token);
-    window.localStorage.setItem("id", response.data.user._id);
-    window.localStorage.setItem("first_name", response.data.user.first_name);
-    window.localStorage.setItem("last_name", response.data.user.last_name);
-    console.log(response.data.user.token);
-    moveToHome();
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/auth/login",
+        data
+      );
+      window.localStorage.setItem("token", response.data.token);
+      window.localStorage.setItem("id", response.data.user._id);
+      window.localStorage.setItem("first_name", response.data.user.first_name);
+      window.localStorage.setItem("last_name", response.data.user.last_name);
+      console.log(response.data.user.token);
+      moveToDashboard();
+    } catch (error) {
+      console.log(error.message)
+      if (error.message ==="Request failed with status code 404")
+        setError("Incorrect Credentials");
+    }
   };
   return (
     <div className="login-form-container">
       <span className="authTitle">Welcome</span>
-      <div className="part">
-        <LabeledInput
-          naming={"email"}
-          Itype={"email"}
-          holder={"please enter your Email"}
-          lab={"Email"}
-          value={data.email}
-          onChange={handleDataChange}
-        />
+      <div className="inputs">
+        <div className="part">
+          <LabeledInput
+            naming={"email"}
+            Itype={"email"}
+            holder={"please enter your Email"}
+            lab={"Email"}
+            value={data.email}
+            onChange={handleDataChange}
+          />
+        </div>
+        <div className="part">
+          <LabeledInput
+            naming={"password"}
+            Itype={"password"}
+            holder={"Please enter your Password"}
+            lab={"Password"}
+            value={data.password}
+            onChange={handleDataChange}
+          />
+        </div>
+        <span className="error">{error}</span>
       </div>
-      <div className="part">
-        <LabeledInput
-          Name={"password"}
-          Itype={"password"}
-          holder={"Please enter your Password"}
-          lab={"Password"}
-          value={data.password}
-          onChange={handleDataChange}
-        />
-      </div>
+      <button className="authButton" onClick={handleLogin}>
+        Sign In
+      </button>
     </div>
   );
 };
