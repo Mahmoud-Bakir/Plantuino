914dd70c711e58f01bf118bd00e34f4c2ec67706
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-25 23:43:16 +0300

    <BE> Updated adminController

--- Server/controllers/admin.controllers.js
+++ Server/controllers/admin.controllers.js
@@ -2,12 +2,23 @@ const User = require("../models/user.model");
 
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find({ userType: { $ne: 3 } }); 
     res.json(users);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+const getSellers = async (req, res) => {
+    try {
+      const users = await User.find({ userType: 1 }); 
+      res.json(users);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
+  
+  module.exports = { getUsers,getSellers };
+  
 
-module.exports = { getUsers };
