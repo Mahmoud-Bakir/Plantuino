045b92e40a35da9a4c178ccd7a6336f43d3bb462
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-26 00:06:05 +0300

    <BE> Updated adminRoutes

--- Server/routes/admin.routes.js
+++ Server/routes/admin.routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const adminController = require("../controllers/admin.controllers");
 
 router.get("/getUsers", adminController.getUsers);
+router.get("/getSellers", adminController.getSellers);
+
 
 module.exports = router;
