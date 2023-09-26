451c1413b4c92fe2084807255db720cad2c7ba16
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-26 13:08:21 +0300

    <FE WEB> Updated Map Component

--- web/src/Components/Map/index.js
+++ web/src/Components/Map/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { Records } from "../Records";
 
-export const Map = ({ records, handleDelete, setter, handleEdit}) => {
+export const Map = ({ records, handleDelete, setter, handleEdit, count }) => {
   const token = localStorage.getItem("token");
-  console.log(records)
+  console.log(records);
 
   async function handleDelete(id) {
     const del = new FormData();
@@ -17,28 +17,49 @@ export const Map = ({ records, handleDelete, setter, handleEdit}) => {
         },
       }
     );
-    setter(records.filter((record) => record.id != id));
+    setter(records.filter((record) => record.id !== id));
   }
 
   return (
     <>
       {records.map((record) => (
-        <Records
-          id={record.id}
-          name={record.name}
-          email={record.email}
-          number={record.phoneNumber}
-          country={record.country}
-          city={record.city}
-          street={record.street}
-          handleDelete={() => handleDelete(record.id)}
-          handleEdit={() => handleEdit(record.id)}
-          key={record.id}
-          all={true}
-        />
+        count === 9 && (
+          <Records
+            id={record.id}
+            userType={record.userType}
+            name={record.name}
+            email={record.email}
+            number={record.phoneNumber}
+            country={record.country}
+            city={record.city}
+            street={record.street}
+            handleDelete={() => handleDelete(record.id)}
+            handleEdit={() => handleEdit(record.id)}
+            key={record.id}
+            all={false}
+          />
+        )
+      ))}
+      {records.map((record) => (
+        count !== 9 && (
+          <Records
+            id={record.id}
+            userType={record.userType}
+            name={record.name}
+            email={record.email}
+            number={record.phoneNumber}
+            country={record.country}
+            city={record.city}
+            street={record.street}
+            handleDelete={() => handleDelete(record.id)}
+            handleEdit={() => handleEdit(record.id)}
+            key={record.id}
+            all={true}
+          />
+        )
       ))}
     </>
   );
 };
-export default Map;
 
+export default Map;
