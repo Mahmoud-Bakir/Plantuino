34e6e42ca48f4a9d75bba3f2b237134281cabb34
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-26 13:06:54 +0300

    <FE WEB> Updated AdminDashboard

--- web/src/Pages/AdminDashboard/index.js
+++ web/src/Pages/AdminDashboard/index.js
@@ -3,13 +3,28 @@ import NavBar from "../../Components/NavBar";
 import "./style.css";
 import SideMenu from "../../Components/SideMenu";
 import Dashboard from "../../Components/Dashboard";
+import Modal from "../../Components/Modal";
+import AddUserForm from "../../Components/AddUserForm";
 
-const AdminDashboard = () => {
+const AdminDashboard = (type) => {
   const [choice, setChoice] = useState(null);
 
   const getChoice = (choice) => {
     setChoice(choice);
   };
+  if (choice === "Add User") {
+    return (
+      <div>
+        <NavBar />
+        <div className="containerContent">
+          <SideMenu handleChoice={getChoice} />
+          <Dashboard title={"All Users"} />
+          <AddUserForm />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
       <NavBar />
