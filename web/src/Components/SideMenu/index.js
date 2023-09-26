d889b230e93cfa75855435c77b9e03c579f3a713
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-26 13:36:58 +0300

    <FE WEB> Updated SideMenu Component

--- web/src/Components/SideMenu/index.js
+++ web/src/Components/SideMenu/index.js
@@ -1,45 +1,62 @@
 import React, { useState } from "react";
-import "./style.css";
 import DashboardIcon from "../../assets/pictures/dashboard.svg";
 import Users from "../../assets/pictures/users.svg";
 import PlantOwners from "../../assets/pictures/PlantOwners.svg";
 import Sellers from "../../assets/pictures/Sellers.svg";
 import AddUser from "../../assets/pictures/addUser.svg";
+import "./style.css";
 
-const SideMenu = ({handleChoice}) => {
+const SideMenu = ({ handleChoice }) => {
+  const [selectedChoice, setSelectedChoice] = useState(null);
 
   const choose = (choice) => {
     handleChoice(choice);
+    setSelectedChoice(choice);
   };
 
   return (
     <div className="sideMenuContainer">
       <div className="sideMenuContent">
-        <div className="categoryContainer" onClick={() => choose("Dashboard")}>
+        <div
+          className={`categoryContainer ${selectedChoice === "Dashboard" ? "selected" : ""}`}
+          onClick={() => choose("Dashboard")}
+        >
           <div className="menuLogoContainer">
             <img src={DashboardIcon} alt="Logo" className="logo" />
           </div>
           <span>Dashboard</span>
         </div>
-        <div className="categoryContainer" onClick={() => choose("All Users")} >
+        <div
+          className={`categoryContainer ${selectedChoice === "All Users" ? "selected" : ""}`}
+          onClick={() => choose("All Users")}
+        >
           <div className="menuLogoContainer">
             <img src={Users} alt="Logo" className="logo" />
           </div>
           <span>All Users</span>
         </div>
-        <div className="categoryContainer" onClick={() => choose("Sellers")}>
+        <div
+          className={`categoryContainer ${selectedChoice === "Sellers" ? "selected" : ""}`}
+          onClick={() => choose("Sellers")}
+        >
           <div className=" menuLogoContainer">
             <img src={Sellers} alt="Logo" className="logo" />
           </div>
           <span>Sellers</span>
         </div>
-        <div className="categoryContainer" onClick={() => choose("Plant Owners")}>
+        <div
+          className={`categoryContainer ${selectedChoice === "Plant Owners" ? "selected" : ""}`}
+          onClick={() => choose("Plant Owners")}
+        >
           <div className=" menuLogoContainer">
             <img src={PlantOwners} alt="Logo" className="logo" />
           </div>
           <span>Plant Owners</span>
-        </div>{" "}
-        <div className="categoryContainer" onClick={() => choose("Add User")}>
+        </div>
+        <div
+          className={`categoryContainer ${selectedChoice === "Add User" ? "selected" : ""}`}
+          onClick={() => choose("Add User")}
+        >
           <div className=" menuLogoContainer">
             <img src={AddUser} alt="Logo" className="logo" />
           </div>
