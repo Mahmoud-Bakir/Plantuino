f79706e640894dcd78d049c2670fe1148bc65875
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-27 10:07:16 +0300

    <BE> Updated userRoutes

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controllers");
 
-
-// router.get("/publicMarket", userController.publicMarket)
-router.get("/personalMarket", userController.personalMarket)
-router.post("/add", userController.addProduct)
-
+router.get("/publicMarket", userController.publicMarket);
+router.get("/personalMarket", userController.personalMarket);
+router.post("/add", userController.addProduct);
+router.put("/updateAddress", userController.updateAddress);
+router.post("/saveMessage", userController.saveMessage);
+router.get("/getMessages", userController.getUserMessages);
+router.post("/answer",userController.answer)
 
 
 
