cf7fefe1eca3a7a68c38f9078d020f2a8156f624
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-27 08:49:48 +0300

    <BE> Added deleteUser API in adminController

--- Server/controllers/admin.controllers.js
+++ Server/controllers/admin.controllers.js
@@ -28,6 +28,20 @@ const getPlantOwners = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({ error: "User ID is required" });
+    }
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json({ message: "User deleted successfully", deletedUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-
-module.exports = { getUsers, getSellers, getPlantOwners };
+module.exports = { getUsers, getSellers, getPlantOwners, deleteUser };
