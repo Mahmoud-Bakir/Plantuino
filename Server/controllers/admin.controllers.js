17b62a2e877358e5af2001ae2b3485840497ac39
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-27 08:49:11 +0300

    <BE> Added getPlantOwners API in adminController

--- Server/controllers/admin.controllers.js
+++ Server/controllers/admin.controllers.js
@@ -2,7 +2,7 @@ const User = require("../models/user.model");
 
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find({ userType: { $ne: 3 } }); 
+    const users = await User.find({ userType: { $ne: 3 } });
     res.json(users);
   } catch (error) {
     console.error(error);
@@ -10,15 +10,24 @@ const getUsers = async (req, res) => {
   }
 };
 const getSellers = async (req, res) => {
-    try {
-      const users = await User.find({ userType: 1 }); 
-      res.json(users);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  };
-  
-  module.exports = { getUsers,getSellers };
-  
+  try {
+    const users = await User.find({ userType: 1 });
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+const getPlantOwners = async (req, res) => {
+  try {
+    const users = await User.find({ userType: 0 });
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
+
+
+module.exports = { getUsers, getSellers, getPlantOwners };
