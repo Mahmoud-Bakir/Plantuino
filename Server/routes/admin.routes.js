56354d93ebe60953963022fbd6e1b934dbc5783a
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-27 08:50:44 +0300

    <BE> Upldated adminRoutes

--- Server/routes/admin.routes.js
+++ Server/routes/admin.routes.js
@@ -4,6 +4,9 @@ const adminController = require("../controllers/admin.controllers");
 
 router.get("/getUsers", adminController.getUsers);
 router.get("/getSellers", adminController.getSellers);
+router.get("/getPlantOwners", adminController.getPlantOwners);
+router.post("/delete", adminController.deleteUser);
+
 
 
 module.exports = router;
