4a751a373bda9c9ee4cf91ede482d9c7e251a7c6
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-28 14:04:20 +0300

    <FE WEB> Updated NavBar Component

--- web/src/Components/NavBar/index.js
+++ web/src/Components/NavBar/index.js
@@ -1,8 +1,15 @@
 import "../NavBar/style.css";
 import Logos from "../../assets/pictures/logo.svg";
 import Name from "../../assets/pictures/logoName.svg";
+import Logout from "../../assets/pictures/logout.svg";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigater = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigater("/");
+  };
   return (
     <div className="navContainer">
       <div className="logoContainer">
@@ -10,6 +17,9 @@ const NavBar = () => {
       </div>
       <div className="logoNameContainer">
         <img src={Name} alt="Logo" className=" logo logoName" />
+      </div>
+      <div className="logoContainer logout">
+        <img src={Logout} alt="Logout" className="logo" onClick={logout} />
       </div>
     </div>
   );
