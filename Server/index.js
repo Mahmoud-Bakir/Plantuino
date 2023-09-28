d4d913a9ab5f78d78ab17694901ca874bb1e3a74
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-28 14:02:38 +0300

    <BE> Introduced CORS in index.js

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-
+const cors = require("cors");
+app.use(cors());
 app.use(express.static("public"));
 
 app.use(express.json());
