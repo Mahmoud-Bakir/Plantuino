1a2916a962ea912f34b67bd08ac88d0887d1c12e
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-28 14:03:46 +0300

    <FE WEB> Updated Map Component

--- web/src/Components/Map/index.js
+++ web/src/Components/Map/index.js
@@ -1,63 +1,64 @@
 import axios from "axios";
 import { Records } from "../Records";
 
-export const Map = ({ records, handleDelete, setter, handleEdit, count }) => {
+export const Map = ({ records, setter, count }) => {
   const token = localStorage.getItem("token");
   console.log(records);
 
-  async function handleDelete(id) {
-    const del = new FormData();
-    del.append("user_id", id);
-    const response = await axios.post(
-      "http://127.0.0.1:8000/api/Admin/delete",
-      del,
-      {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
+  const remove = async (id) => {
+   const data={id:id}
+   console.log(data)
+    await axios.post("http://192.168.1.5:8000/admin/delete", data, {
+      headers: {
+        Authorization: "Bearer " + token,
       }
-    );
+    });
     setter(records.filter((record) => record.id !== id));
-  }
+  };
+  const edit = async (id) => {
+    console.log("edit");
+  };
 
   return (
     <>
-      {records.map((record) => (
-        count === 9 && (
-          <Records
-            id={record.id}
-            userType={record.userType}
-            name={record.name}
-            email={record.email}
-            number={record.phoneNumber}
-            country={record.country}
-            city={record.city}
-            street={record.street}
-            handleDelete={() => handleDelete(record.id)}
-            handleEdit={() => handleEdit(record.id)}
-            key={record.id}
-            all={false}
-          />
-        )
-      ))}
-      {records.map((record) => (
-        count !== 9 && (
-          <Records
-            id={record.id}
-            userType={record.userType}
-            name={record.name}
-            email={record.email}
-            number={record.phoneNumber}
-            country={record.country}
-            city={record.city}
-            street={record.street}
-            handleDelete={() => handleDelete(record.id)}
-            handleEdit={() => handleEdit(record.id)}
-            key={record.id}
-            all={true}
-          />
-        )
-      ))}
+      {records.map(
+        (record) =>
+          count === 9 && (
+            <Records
+              id={record.id}
+              userType={record.userType}
+              name={record.name}
+              email={record.email}
+              number={record.phoneNumber}
+              country={record.country}
+              city={record.city}
+              street={record.street}
+              handleDelete={() => remove(record.id)}
+              handleEdit={() => edit(record.id)}
+              key={record.id}
+              all={false}
+            />
+          )
+      )}
+      {records.map(
+        (record) =>
+          count !== 9 && (
+            <Records
+              id={record.id}
+              userType={record.userType}
+              name={record.name}
+              email={record.email}
+              number={record.phoneNumber}
+              country={record.country}
+              city={record.city}
+              street={record.street}
+              handleDelete={() => remove(record.id)}
+              handleEdit={() => edit(record.id)}
+              key={record.id}
+              all={true}
+            />
+          )
+      )}
     </>
   );
 };
