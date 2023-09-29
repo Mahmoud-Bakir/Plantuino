84822f0501766abd1732b89b1c33248c94261b55
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-29 16:31:18 +0300

    <BE> updated arduinoRoutes

--- Server/routes/arduino.routes.js
+++ Server/routes/arduino.routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const arduinoController = require('../controllers/arduino.controllers');
 
-router.post('/data', arduinoController.sendData);
-router.post('/deleteData', arduinoController.deleteAllData);
+router.post('/sendData', arduinoController.sendData);
+router.get('/deleteData', arduinoController.deleteAllData);
+router.get('/getData', arduinoController.getData);
 
 
 module.exports = router;
