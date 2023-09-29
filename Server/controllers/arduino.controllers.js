97a87b43132a3b650da0ed204d8225abb313a38f
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-29 16:21:20 +0300

    <BE> Added SendData API to arduinoController

--- Server/controllers/arduino.controllers.js
+++ Server/controllers/arduino.controllers.js
@@ -1,8 +1,21 @@
-    const sendData = (req, res) => {
-      const data = req.body;
-      console.log('Received data from Arduino:', data);
-      res.json({ receivedData: data });
-    }
-  
-  module.exports = {sendData};
-  
+
+const Data = require("../models/data.model");
+  const sendData = async (req, res) => {
+    const data = req.body;
+    console.log('Received data from Arduino:', data);
+    const newData = new Data({
+      moisture: data.moisture_value,
+      sunlight: data.ldr_value,
+    });
+    newData.save()
+      .then(savedData => {
+        console.log('Data saved to database:', savedData);
+        res.json({ receivedData: data, savedData: savedData });
+      })
+      .catch(error => {
+        console.error('Error saving data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      });
+  }
+
+module.exports = {sendData};
