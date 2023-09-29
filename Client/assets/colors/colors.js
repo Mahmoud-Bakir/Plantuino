7a8eef8df66f56a9d2e0f86aa3864ac4dd1e8902
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-29 06:18:24 +0300

    <FE APP> Updated colors file

--- Client/assets/colors/colors.js
+++ Client/assets/colors/colors.js
@@ -2,8 +2,15 @@ const Colors = {
     Green: '#38D13E',
     White: '#ffffff',
     Grey: '#6A6A6A',
+    DarkGrey:"#333333",
     Blue: '#ABCFFF',
-    Black:'#000000'
+    Black:'#000000',
+    Red:'#FF0000',
+    LightGrey:'#CCCCCC',
+    LigherGrey:'#F5F5F5',
+    LightBlue:"#dae0e7",
+    GreyTransparent:"rgba(106, 106, 106, 0.4)"
+
   };
   
   export default Colors;
