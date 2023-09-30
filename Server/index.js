5e7525080dcf96882b70c48fc18681543ba1b5d7
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-30 17:30:48 +0300

    <BE> Updated index file

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 app.use(cors());
-app.use(express.static("public"));
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.json());
 const mongooseConnect = require("./configs/mongoDB.connect");
