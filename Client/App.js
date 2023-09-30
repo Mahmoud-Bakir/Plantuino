a941630a18f033bbe4a90298f1b0ff663caabdc8
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-30 22:45:28 +0300

    <FE APP> Updated App.js file

--- Client/App.js
+++ Client/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, Alert } from "react";
+import React, { useEffect } from "react";
+import io from "socket.io-client";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import RegisterationScreen from "./screens/RegisterationScreen";
 import SigninScreen from "./screens/SigninScreen";
 import { StatusBar } from "expo-status-bar";
@@ -9,61 +9,50 @@ import BottomTabNavigator from "./Components/BottomTabNavigator";
 import AddProductScreen from "./screens/AddProductScreen";
 import ResultScreen from "./screens/ResultScreen";
 import registerNNPushToken from "native-notify";
-
+import store from "./Redux/Store";
+import { Provider } from "react-redux";
 import "expo-dev-client";
 import "firebase/auth";
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
 function Home() {
   return <BottomTabNavigator />;
 }
 
 export default function App() {
-  registerNNPushToken(12377, "YCjAsF4USBdjSLbUwETH8H");
-
-  async function requestUserPermission() {
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-    if (enabled) {
-      console.log("Authorization status:", authStatus);
-    }
-  }
 
   return (
-    <NavigationContainer>
-      <StatusBar backgroundColor="black" />
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SigninScreen"
-          component={SigninScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="RegisterScreen"
-          component={RegisterationScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="AddProductScreen"
-          component={AddProductScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ResultScreen"
-          component={ResultScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar backgroundColor="black" />
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SigninScreen"
+            component={SigninScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="RegisterScreen"
+            component={RegisterationScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="AddProductScreen"
+            component={AddProductScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ResultScreen"
+            component={ResultScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
