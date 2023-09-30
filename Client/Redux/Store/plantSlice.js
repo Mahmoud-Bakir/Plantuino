57d25beb9d0e11d741ea3accef8e51dc0b9b86c3
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-30 12:50:10 +0300

    <FE APP> Updated plantSlice in Redux Store

--- Client/Redux/Store/plantSlice.js
+++ Client/Redux/Store/plantSlice.js
@@ -3,6 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   name: "",
   image: "",
+  maxLight: null,
+  maxMoisture: null,
+  minLight: null,
+  minMoisture: null,
+  defined: false,
 };
 
 const plantSlice = createSlice({
@@ -15,11 +20,38 @@ const plantSlice = createSlice({
     setPlantImage(state, action) {
       state.image = action.payload;
     },
+    setPlantDetails(state, action) {
+      const {
+        maxLight,
+        maxMoisture,
+        minLight,
+        minMoisture,
+        name
+      } = action.payload;
+      state.maxLight = maxLight;
+      state.maxMoisture = maxMoisture;
+      state.minLight = minLight;
+      state.minMoisture = minMoisture;
+      state.name = name;
+      state.defined = true; 
+    },
+    setDefinedTrue(state) {
+      state.defined = true;
+    },
   },
 });
 
-export const { setPlantName, setPlantImage } = plantSlice.actions;
-export default plantSlice.reducer;
+export const {
+  setPlantName,
+  setPlantImage,
+  setPlantDetails,
+  setDefinedTrue,
+} = plantSlice.actions;
 
 export const selectPlantName = (state) => state.plant.name;
 export const selectPlantImage = (state) => state.plant.image;
+export const selectPlantDefined = (state) => state.plant.defined;
+export const selectPlantDetails = (state) => state.plant;
+
+
+export default plantSlice.reducer;
