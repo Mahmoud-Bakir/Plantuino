6dfac4952f5f140e60eb199f29268b667830a319
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-09-30 22:50:31 +0300

    <FE WEB> Added AuthenticationPage

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
+import AdminDashboard from "./Pages/AdminDashboard";
+import AuthenticationPage from "./Pages/AuthenticationPage";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<AuthenticationPage />} />
           <Route path="/LandingPage" element={<LandingPage />} />
+          <Route path="/Dashboard" element={<AdminDashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
