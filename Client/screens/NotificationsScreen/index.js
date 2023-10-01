fe8ffcf9edd2fb9bd158f1b5a180c8f9d98647f7
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-10-01 21:49:19 +0300

    <FE APP> Created NotificationScreen

--- Client/screens/NotificationsScreen/index.js
+++ Client/screens/NotificationsScreen/index.js
@@ -1,20 +1,51 @@
-import React, { useState, useEffect } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
+import axios from "axios";
 import {
+  getNotificationInbox,
   getIndieNotificationInbox,
   deleteIndieNotificationInbox,
 } from "native-notify";
+import { useSelector, useDispatch } from "react-redux";
+import { selectAuthState } from "../../Redux/Store/authSlice";
+import { ScrollView } from "react-native-gesture-handler";
 
 export const NotificationsScreen = () => {
   const [data, setData] = useState([]);
-  useEffect(async () => {
-    let notifications = await getIndieNotificationInbox(
-      "put your Indie Push Sub ID here as a string",
-      12377,
-      "YCjAsF4USBdjSLbUwETH8H"
-    );
-    console.log("notifications: ", notifications);
-    setData(notifications);
-  }
-  , []);
-  return <View style={{ backgroundColor: "plum", flex: 1 }}></View>;
+  const auth = useSelector(selectAuthState);
+  const _id = auth._id;
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let notifications = await getIndieNotificationInbox(
+          _id,
+          12747,
+          "BDt99Jcmi6Wq2atbqo1sGR"
+        );
+        console.log("notifications: ", notifications);
+        setData(notifications);
+        console.log(notifications);
+      } catch (error) {
+        console.error("Error fetching :", error);
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <ScrollView>
+        {data.map((notification, index) => (
+          <View
+            key={index}
+            style={{ marginBottom: 10, padding: 10, backgroundColor: "white" }}
+          >
+            <Text>Date: {notification.date}</Text>
+            <Text>Message: {notification.message}</Text>
+            <Text>Notification: {notification.notification}</Text>
+            <Text>Title: {notification.title}</Text>
+          </View>
+        ))}
+    </ScrollView>
+  );
 };
