43e4c72bdda27f9b9b41aa20683a9b205b836c17
Mahmoud-Bakir <mahmoudbakir21@gmail.com>
2023-10-01 10:11:25 +0300

    <BE> Updated userRoutes

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -8,11 +8,11 @@ router.post("/add", userController.addProduct);
 router.put("/updateAddress", userController.updateAddress);
 router.post("/saveMessage", userController.saveMessage);
 router.get("/getMessages", userController.getUserMessages);
-router.post("/answer",userController.answer)
-router.post("/getPreferences",userController.getPreferences)
-router.post("/updatePlants",userController.updatePlants)
-router.post("/deleteProduct",userController.deleteProduct)
-router.post("/editProduct",userController.editProduct)
-
+router.post("/answer", userController.answer);
+router.post("/getPreferences", userController.getPreferences);
+router.post("/updatePlants", userController.updatePlants);
+router.post("/deleteProduct", userController.deleteProduct);
+router.post("/editProduct", userController.editProduct);
+router.post("/search", userController.searchProducts);
 
 module.exports = router;
